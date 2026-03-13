d5fa101e8745e1cf703d6891bca55443195cbb81
John Doe <johndoe@example.com>
2026-03-13 15:50:58 +0800

    1

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ echo "TARGET_DEVICE: $TARGET_DEVICE"
 
 if [ $KSU_ENABLE -eq 1 ]; then
     echo "KSU is enabled"
-    curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash
+    curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
 else
     echo "KSU is disabled"
 fi
