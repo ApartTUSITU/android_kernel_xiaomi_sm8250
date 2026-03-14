89a3ba5d930fa899cf5260c2302dd5bbaacc3335
John Doe <johndoe@example.com>
2026-03-14 16:12:33 +0800

    patch avc and enable rekernel

--- build.sh
+++ build.sh
@@ -122,7 +122,7 @@ if [ $KSU_ENABLE -eq 1 ]; then
     scripts/config --file out/.config \
     -e KSU \
     -e THREAD_INFO_IN_TASK \
-    -e KPM
+    -e REKERNEL
 else
     scripts/config --file out/.config -d KSU
 fi
